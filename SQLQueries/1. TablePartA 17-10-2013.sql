09cbf04e0751229b1efcca7d356e10b986d19f85
rubyverma <rubyverma2080@gmail.com>
2013-10-17 14:08:39 -0400

    'Create new tables[Part A]

---
+++ SQLQueries/1. TablePartA 17-10-2013.sql
@@ -0,0 +1,129 @@
+/*
+SQLyog Community v11.21 (64 bit)
+MySQL - 5.6.12-log : Database - scheduler
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`scheduler` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `scheduler`;
+
+/*Table structure for table `announcement` */
+
+DROP TABLE IF EXISTS `announcement`;
+
+CREATE TABLE `announcement` (
+  `announcementId` int(50) NOT NULL AUTO_INCREMENT,
+  `officialId` int(50) NOT NULL,
+  `announcementHeader` varchar(100) NOT NULL,
+  `announcementDescription` varchar(255) DEFAULT NULL,
+  `announcementDate` datetime NOT NULL,
+  PRIMARY KEY (`announcementId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `appointment` */
+
+DROP TABLE IF EXISTS `appointment`;
+
+CREATE TABLE `appointment` (
+  `appointmentId` int(50) NOT NULL AUTO_INCREMENT,
+  `departmentTimeId` int(50) NOT NULL,
+  `userId` int(50) NOT NULL,
+  `officialId` int(50) DEFAULT NULL,
+  `purposeOfVisit` varchar(255) DEFAULT NULL,
+  `startTime` datetime NOT NULL,
+  `endTime` datetime NOT NULL,
+  `meetingFinished` varchar(1) DEFAULT NULL,
+  `meetingNotes` text,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`appointmentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `campus` */
+
+DROP TABLE IF EXISTS `campus`;
+
+CREATE TABLE `campus` (
+  `campusId` int(50) NOT NULL AUTO_INCREMENT,
+  `clientId` int(50) NOT NULL,
+  `campusName` varchar(50) NOT NULL,
+  `campusAddress` varchar(100) DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`campusId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `categoryId` int(50) NOT NULL AUTO_INCREMENT,
+  `officialId` int(50) NOT NULL,
+  `categoryName` varchar(50) NOT NULL,
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `client` */
+
+DROP TABLE IF EXISTS `client`;
+
+CREATE TABLE `client` (
+  `clientId` int(10) NOT NULL AUTO_INCREMENT,
+  `clientName` varchar(50) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `logo` varchar(100) DEFAULT NULL,
+  `phone1` varchar(15) NOT NULL,
+  `phone2` varchar(15) DEFAULT NULL,
+  `phone3` varchar(15) DEFAULT NULL,
+  `contactPerson` varchar(50) DEFAULT NULL,
+  `website` varchar(100) DEFAULT NULL,
+  `token` varchar(100) NOT NULL,
+  `emailVerified` tinyint(1) DEFAULT NULL,
+  `dateJoined` datetime NOT NULL,
+  PRIMARY KEY (`clientId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `departmentId` int(50) NOT NULL AUTO_INCREMENT,
+  `campusId` int(50) NOT NULL,
+  `departmentName` varchar(50) NOT NULL,
+  `departmentHod` varchar(50) NOT NULL,
+  `contactInfo` int(15) NOT NULL,
+  `departmentDescription` varchar(255) DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`departmentId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Table structure for table `departmenttimeslot` */
+
+DROP TABLE IF EXISTS `departmenttimeslot`;
+
+CREATE TABLE `departmenttimeslot` (
+  `departmentTimeId` int(50) NOT NULL AUTO_INCREMENT,
+  `departmentId` int(50) NOT NULL,
+  `timeslotId` int(50) NOT NULL,
+  `weekdays` varchar(7) NOT NULL,
+  `capacity` int(5) NOT NULL,
+  PRIMARY KEY (`departmentTimeId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
