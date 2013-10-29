bb7ee4eca28b2c1b68ad445d4470c0c2efa0ce55
rubyverma <rubyverma2080@gmail.com>
2013-10-28 21:51:20 -0400

    DB New Version with required "User Announcement Table"

---
+++ SQLQueries/Scheduler DB v.1.0.sql
@@ -0,0 +1,253 @@
+/*
+SQLyog Community v11.21 (64 bit)
+MySQL - 5.6.12-log : Database - scheduler
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`scheduler` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `scheduler`;
+
+/*Table structure for table `announcement` */
+
+DROP TABLE IF EXISTS `announcement`;
+
+CREATE TABLE `announcement` (
+  `announcementId` int(50) NOT NULL AUTO_INCREMENT,
+  `officialId` int(50) NOT NULL,
+  `announcementHeader` varchar(100) NOT NULL,
+  `announcementDescription` varchar(255) DEFAULT NULL,
+  `gcmMessageId` int(100) NOT NULL,
+  `announcementDate` datetime NOT NULL,
+  PRIMARY KEY (`announcementId`),
+  UNIQUE KEY `UNIQUE` (`gcmMessageId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `appointment` */
+
+DROP TABLE IF EXISTS `appointment`;
+
+CREATE TABLE `appointment` (
+  `appointmentId` int(50) NOT NULL AUTO_INCREMENT,
+  `departmentTimeId` int(50) NOT NULL,
+  `userId` int(50) NOT NULL,
+  `officialId` int(50) DEFAULT NULL,
+  `purposeOfVisit` varchar(255) DEFAULT NULL,
+  `startTime` time DEFAULT NULL,
+  `endTime` time DEFAULT NULL,
+  `meetingFinished` varchar(1) DEFAULT NULL,
+  `meetingNotes` text,
+  `dateCreated` datetime NOT NULL,
+  `appointmentDate` date NOT NULL,
+  PRIMARY KEY (`appointmentId`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `campus` */
+
+DROP TABLE IF EXISTS `campus`;
+
+CREATE TABLE `campus` (
+  `campusId` int(50) NOT NULL AUTO_INCREMENT,
+  `clientId` int(50) NOT NULL,
+  `campusName` varchar(50) NOT NULL,
+  `campusAddress` varchar(100) DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`campusId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `category` */
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category` (
+  `categoryId` int(50) NOT NULL AUTO_INCREMENT,
+  `officialId` int(50) NOT NULL,
+  `categoryName` varchar(50) NOT NULL,
+  PRIMARY KEY (`categoryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `client` */
+
+DROP TABLE IF EXISTS `client`;
+
+CREATE TABLE `client` (
+  `clientId` int(10) NOT NULL AUTO_INCREMENT,
+  `clientName` varchar(50) NOT NULL,
+  `userName` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `memo` varchar(255) DEFAULT NULL,
+  `logo` varchar(100) DEFAULT NULL,
+  `phone1` varchar(15) NOT NULL,
+  `phone2` varchar(15) DEFAULT NULL,
+  `phone3` varchar(15) DEFAULT NULL,
+  `contactPerson` varchar(50) DEFAULT NULL,
+  `website` varchar(100) DEFAULT NULL,
+  `token` varchar(100) NOT NULL,
+  `emailVerified` tinyint(1) DEFAULT NULL,
+  `dateJoined` datetime NOT NULL,
+  PRIMARY KEY (`clientId`),
+  UNIQUE KEY `UNIQUE` (`userName`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `department` */
+
+DROP TABLE IF EXISTS `department`;
+
+CREATE TABLE `department` (
+  `departmentId` int(50) NOT NULL AUTO_INCREMENT,
+  `campusId` int(50) NOT NULL,
+  `departmentName` varchar(50) NOT NULL,
+  `departmentHod` varchar(50) NOT NULL,
+  `contactInfo` int(15) NOT NULL,
+  `departmentDescription` varchar(255) DEFAULT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`departmentId`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `departmenttimeslot` */
+
+DROP TABLE IF EXISTS `departmenttimeslot`;
+
+CREATE TABLE `departmenttimeslot` (
+  `departmentTimeId` int(50) NOT NULL AUTO_INCREMENT,
+  `departmentId` int(50) NOT NULL,
+  `timeslotId` int(50) NOT NULL,
+  `weekdays` varchar(7) NOT NULL,
+  `capacity` int(5) NOT NULL,
+  PRIMARY KEY (`departmentTimeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `faq` */
+
+DROP TABLE IF EXISTS `faq`;
+
+CREATE TABLE `faq` (
+  `faqId` int(50) NOT NULL AUTO_INCREMENT,
+  `categoryId` int(50) NOT NULL,
+  `officialId` int(50) NOT NULL,
+  `faqQuestion` varchar(255) NOT NULL,
+  `faqAnswer` varchar(255) NOT NULL,
+  `dateCreated` datetime NOT NULL,
+  PRIMARY KEY (`faqId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `generaluser` */
+
+DROP TABLE IF EXISTS `generaluser`;
+
+CREATE TABLE `generaluser` (
+  `userId` int(50) NOT NULL AUTO_INCREMENT,
+  `clientId` int(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `token` varchar(100) NOT NULL,
+  `emailVerified` tinyint(1) DEFAULT NULL,
+  `gcmRegId` text,
+  PRIMARY KEY (`userId`),
+  KEY `UNIQUE` (`username`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `notification` */
+
+DROP TABLE IF EXISTS `notification`;
+
+CREATE TABLE `notification` (
+  `notificationId` int(50) NOT NULL AUTO_INCREMENT,
+  `officialId` int(50) NOT NULL,
+  `userId` int(50) NOT NULL,
+  `gcmMessageId` varchar(100) DEFAULT NULL,
+  `notificationHeader` varchar(100) DEFAULT NULL,
+  `notificationDescription` varchar(255) NOT NULL,
+  `readByUser` tinyint(1) NOT NULL,
+  `notificationDate` datetime NOT NULL,
+  PRIMARY KEY (`notificationId`),
+  UNIQUE KEY `UNIQUE` (`gcmMessageId`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `officialuser` */
+
+DROP TABLE IF EXISTS `officialuser`;
+
+CREATE TABLE `officialuser` (
+  `officialId` int(50) NOT NULL AUTO_INCREMENT,
+  `departmentId` int(50) NOT NULL,
+  `roleId` int(50) NOT NULL,
+  `officialName` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `dateJoined` date NOT NULL,
+  `lastLogin` datetime NOT NULL,
+  PRIMARY KEY (`officialId`),
+  KEY `UNIQUE` (`officialName`,`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `roles` */
+
+DROP TABLE IF EXISTS `roles`;
+
+CREATE TABLE `roles` (
+  `roleId` int(50) NOT NULL AUTO_INCREMENT,
+  `clientId` int(50) NOT NULL,
+  `roleName` varchar(50) NOT NULL,
+  `privilege` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`roleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `test_user` */
+
+DROP TABLE IF EXISTS `test_user`;
+
+CREATE TABLE `test_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `timeslot` */
+
+DROP TABLE IF EXISTS `timeslot`;
+
+CREATE TABLE `timeslot` (
+  `timeslotId` int(50) NOT NULL AUTO_INCREMENT,
+  `startTime` time NOT NULL,
+  `stopTime` time NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`timeslotId`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+/*Table structure for table `userannouncements` */
+
+DROP TABLE IF EXISTS `userannouncements`;
+
+CREATE TABLE `userannouncements` (
+  `userAnnouncementId` int(11) NOT NULL AUTO_INCREMENT,
+  `announcementId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  PRIMARY KEY (`userAnnouncementId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
