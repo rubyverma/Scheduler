949ce085f4ff4590b2484c9528f8f00805093b5e
rubyverma <rubyverma2080@gmail.com>
2013-11-04 21:58:16 -0500

    As a student I should be able to book an appointment-BL [#Finishes 59790340]

--- SchedulerWeb/src/main/java/com/scheduler/controllers/RestController.java
+++ SchedulerWeb/src/main/java/com/scheduler/controllers/RestController.java
@@ -1,9 +1,11 @@
 package com.scheduler.controllers;
  
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+ 
+import lombok.extern.slf4j.Slf4j;
+ 
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,20 +14,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+ 
+import com.scheduler.models.Appointment;
 import com.scheduler.models.Campus;
 import com.scheduler.models.Department;
 import com.scheduler.models.DepartmentTimeslotLinkage;
-import com.scheduler.models.Departmenttimeslot;
-import com.scheduler.models.User;
 import com.scheduler.models.Utility;
-import com.scheduler.request.CommonResponse;
+import com.scheduler.services.AppointmentService;
 import com.scheduler.services.CampusService;
 import com.scheduler.services.DepartmentService;
 import com.scheduler.services.DepartmentTimeslotService;
  
 @RequestMapping("/api")
 @Controller
+@Slf4j
 public class RestController {
  
     protected static final String JSON_CONTENT = "application/json";
@@ -36,6 +38,8 @@ public class RestController {
     private DepartmentService departmentService;
     @Autowired(required = true)
     private DepartmentTimeslotService departmentTimeslotService;
+	@Autowired(required = true)
+	private AppointmentService appointmentService;
        
     
     @RequestMapping(value = "/getCampusByClient/{id}", method = RequestMethod.GET, produces = JSON_CONTENT)
@@ -52,15 +56,30 @@ public class RestController {
         return departments;
     }
     
-    
     @RequestMapping(value = "/getDepartmentTimeslot/{id}/{dt}", method = RequestMethod.GET, produces = JSON_CONTENT)
     @ResponseBody
     public List<DepartmentTimeslotLinkage> getDepartmentTimeslot(@PathVariable int id, @PathVariable String dt) throws java.text.ParseException, ParseException {
-    	Date d = null;
-    	d = new SimpleDateFormat("yyyy-mm-dd").parse(dt);
 		Utility u = new Utility(id, dt);
 		System.out.println(u.getDepartmentId() + " - "+ u.getAppointmentDate());
 		List<DepartmentTimeslotLinkage> availableTimeslots = departmentTimeslotService.timeslotByDepartment(u);
     	return availableTimeslots;
     }
+    
+    @RequestMapping(value = "/saveappointment", method = RequestMethod.POST, produces = JSON_CONTENT)
+    @ResponseBody
+    public Map<String, String> saveAppointment(@RequestBody Appointment appointment) {
+    	int i = -1;
+		try {
+			i = appointmentService.saveAppointment(appointment);
+		} catch (Exception e) {
+			log.debug(e.getMessage());
+		}
+		Map<String, String> result = new HashMap<String, String>();
+		if(i == -1) {
+			result.put("result", "fail");
+		} else {
+			result.put("result", "success");
+		}
+    	return result;
+    }    
 }
