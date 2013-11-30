c59152f8d5ab334dc76de3bcc67a144928a4fb8e
Sonny Raju <sonnykr@gmail.com>
2013-11-30 17:22:21 -0500

    Unit testing and bug fix for start appointment [Fixes #61631464]

--- SchedulerWeb/src/test/resources/schema.sql
+++ SchedulerWeb/src/test/resources/schema.sql
@@ -29,3 +29,55 @@ CREATE TABLE IF NOT EXISTS `departmenttimeslot` (
 
 INSERT INTO `timeslot` (`timeslotId`, `clientId`, `startTime`, `stopTime`, `description`) VALUES
 (1, 1, '09:00:00', '13:00:00', 'New timeslot');
+
+DROP table if exists appointment;
+CREATE TABLE IF NOT EXISTS `appointment` (
+  `appointmentId` int(50) NOT NULL AUTO_INCREMENT,
+  `departmentTimeId` int(50) NOT NULL,
+  `userId` int(50) NOT NULL,
+  `officialId` int(50) DEFAULT NULL,
+  `purposeOfVisit` varchar(255) DEFAULT NULL,
+  `startTime` time DEFAULT NULL,
+  `endTime` time DEFAULT NULL,
+  `meetingFinished` varchar(1) DEFAULT NULL,
+  `meetingNotes` text,
+  `dateCreated` datetime NOT NULL,
+  `appointmentDate` date NOT NULL,
+  PRIMARY KEY (`appointmentId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+INSERT INTO `appointment` (`appointmentId`, `departmentTimeId`, `userId`, `officialId`, `purposeOfVisit`, `startTime`, `endTime`, `meetingFinished`, `meetingNotes`, `dateCreated`, `appointmentDate`) VALUES
+(1, 1, 1, 3, 'Course Enquiry', '00:00:00', '00:00:00', '', '', '2013-11-11 08:16:45', '2013-11-10');
+
+CREATE TABLE IF NOT EXISTS `generaluser` (
+  `userId` int(50) NOT NULL AUTO_INCREMENT,
+  `clientId` int(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `dob` date DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `gender` varchar(10) DEFAULT NULL,
+  `token` varchar(100) NOT NULL,
+  `emailVerified` tinyint(1) DEFAULT NULL,
+  `gcmRegId` text,
+  PRIMARY KEY (`userId`),
+  KEY `UNIQUE` (`username`,`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+INSERT INTO `generaluser` (`userId`, `clientId`, `username`, `password`, `firstName`, `lastName`, `email`, `dob`, `address`, `gender`, `token`, `emailVerified`, `gcmRegId`) VALUES
+(1, 1, 'dkaith', 'cc1234', 'Kaith', 'Devon', 'dkaith@ccollege.com', '1985-12-14', '22 Progress Ave', 'Female', 'ab12', 1, '');
+
+CREATE TABLE IF NOT EXISTS `notification` (
+  `notificationId` int(50) NOT NULL AUTO_INCREMENT,
+  `officialId` int(50) NOT NULL,
+  `userId` int(50) NOT NULL,
+  `gcmMessageId` varchar(100) DEFAULT NULL,
+  `notificationHeader` varchar(100) DEFAULT NULL,
+  `notificationDescription` varchar(255) NOT NULL,
+  `readByUser` tinyint(1) NOT NULL,
+  `notificationDate` datetime NOT NULL,
+  PRIMARY KEY (`notificationId`),
+  UNIQUE KEY `UNIQUE` (`gcmMessageId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
