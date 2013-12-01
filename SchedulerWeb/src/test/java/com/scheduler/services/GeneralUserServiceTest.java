7a1e2421419d441aea41015ebad72e39f6eeedd4
sanket18 <techrockers@yahoo.co.in>
2013-11-30 22:05:52 -0500

    Testing (As a student I want to register to the system[  Verifying  student through Email]) completed

--- SchedulerWeb/src/test/java/com/scheduler/services/GeneralUserServiceTest.java
+++ SchedulerWeb/src/test/java/com/scheduler/services/GeneralUserServiceTest.java
@@ -15,5 +15,27 @@ public class GeneralUserServiceTest extends BaseTest{
 	@Autowired(required=true)
 	private GeneralUserService generalUserService;
 	
+	@Test
+	public void testGeneralUserRegistration()
+	{
+		Date date= new Date(1985,12,12);
+		
+		GeneralUser generaluser= new GeneralUser();
+		generaluser.setClientId(1);
+		generaluser.setUsername("testUser");
+		generaluser.setPassword("testPassword");
+		generaluser.setFirstName("testFirstName");
+		generaluser.setLastName("testLastName");
+		generaluser.setEmail("techrockers@yahoo.co.in");
+		//generaluser.setDob(date);
+		generaluser.setAddress("test Address");
+		generaluser.setGender("Male");
+		generaluser.setTocken("123456");
+		generaluser.setGcmRegId("test GCM ID");
+		int testResult=generalUserService.saveUser(generaluser);
+		
+		assertTrue("Failed to Register General User",testResult==1);
+	}
+
 	
 }
