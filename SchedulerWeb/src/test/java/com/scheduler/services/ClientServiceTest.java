7806b0668f917a4cc92d9eb6c984d96094b2dd4b
sanket18 <techrockers@yahoo.co.in>
2013-11-30 22:23:53 -0500

    Testing ( As a new client I want to register to the system ) Completed

--- SchedulerWeb/src/test/java/com/scheduler/services/ClientServiceTest.java
+++ SchedulerWeb/src/test/java/com/scheduler/services/ClientServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.BadSqlGrammarException;
 
 import com.scheduler.BaseTest;
+import com.scheduler.models.Client;
 import com.scheduler.models.DepartmentStatistics;
 
 public class ClientServiceTest extends BaseTest {
@@ -15,5 +16,26 @@ public class ClientServiceTest extends BaseTest {
 	@Autowired(required = true)
 	private ClientService clientService;
 
-	
+	@Test
+	public void TestClientRegistration()
+	{
+		Client client= new Client();
+		client.setClientName("testClient");
+		client.setUserName("test");
+		client.setPassword("test");
+		client.setEmail("techrockers@yahoo.co.in");
+		client.setAddress("test address");
+		client.setMemo("test memo");
+		client.setPhone1("test phone1");
+		client.setPhone2("test phone2");
+		client.setPhone3("test phone3");
+		client.setLogo("test_logo.jpg");
+		client.setContactPerson("Test Person");
+		client.setWebsite("www.testwebsite.com");
+		client.setToken("123456");
+		int testResult= clientService.saveClient(client);
+		assertTrue("Failed to Register a Client",testResult==1);
+		
+	}
+
 }
